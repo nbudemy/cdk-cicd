021a222364bf4d770a1f43a169957d13799b8926
Neil Bottomley <neil.bottomley@skybettingandgaming.com>
2024-09-30 11:03:15 +0100

    Add pipeline

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'myFirstPipeline', {
+      pipelineName: 'myFirstPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('nbudemy/cdk-cicd', 'cicd-practice'),
+        commands: [
+          'npm install',
+          'npx cdk synth'
+        ]
+      })
+    })
   }
 }
